d985e97923f423c5844bc41a17dc260b59c83e2c
Joao-we <jm4113385@gmail.com>
2025-11-21 14:29:55 -0300

    segundo commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 import express from "express";
 import Agendamento from "./agendamentos.js";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 
@@ -9,6 +10,10 @@ app.use(cors());
 
 // Middleware para ler JSON do corpo da requisição
 app.use(express.json());
+
+const_dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "public")));
 
 // GET - Consulta os dados cadastrados
 app.get ("/agendamentos", async (req, res)=>{
